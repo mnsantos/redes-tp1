bc5edc74ee9110a41adabaeb941fe8a4f23d7aca
Jonathan Melnik <jonathanmelnik@gmail.com>
2014-04-28 15:36:51 -0300

    tool

---
+++ arpinglisten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from scapy.all import *
+import json
+
+counter = 0
+
+def monitor_callback(pkt):
+  global counter
+  #print "src " + pkt.src
+  #print "dst " + pkt.dst
+  #if pkt.op == 1:
+  #  print "op who-has"
+  #else:
+  #  print "op is-at"
+  #print pkt.show()
+  p = {"src":pkt.src, "dst":pkt.dst, "psrc":pkt.psrc, "pdst":pkt.pdst, "op":pkt.op, "hwsrc":pkt.hwsrc, "hwdst":pkt.hwdst}
+  jsonObj = json.dumps(p, ensure_ascii=False)
+  f = open('out.txt', 'a')
+  f.write(",")
+  f.write(jsonObj)
+  f.write("\n")
+  f.close()
+  counter = counter+1
+  print "wrote ARPs: " + str(counter)
+  
+if __name__ == '__main__':
+  sniff(prn=monitor_callback, filter="arp", store = 0)
+  
